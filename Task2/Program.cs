f19f1a44f119fd23254443af091f514abb624d43
Mariya <marisha_1793@mail.ru>
2022-12-15 00:08:49 +0700

    вторая задача

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int sum = TheSumOfDigitsInANumber(N);
+Console.WriteLine(sum);
+
+int TheSumOfDigitsInANumber(int N)
+{
+    int sum = 0;
+    for(int newN = N; newN > 0;)
+    {
+        int number = newN % 10;
+        sum = sum + number;
+        newN = newN /10;
+    }
+
+    return sum;
+}
