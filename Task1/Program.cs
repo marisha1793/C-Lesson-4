0d0a87a55143de39dea0c5450c4c02ece1b5a8af
Mariya <marisha_1793@mail.ru>
2022-12-14 23:29:20 +0700

    первая задача

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два 
+// числа (A и B) и возводит число A в натуральную степень B. Не использовать метод Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите два числа: ");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+
+int numberA = RaisingTheNumberAToThePowerOfB (A, B);
+
+Console.WriteLine(numberA);
+
+int RaisingTheNumberAToThePowerOfB (int A, int B)
+{
+    int numberA = 1;
+    for (int count = 1; count <= B; count++)
+    {
+        numberA = numberA*A;
+    }
+
+    return numberA;
+}
